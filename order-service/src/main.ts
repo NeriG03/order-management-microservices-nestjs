017a2d0938bc61642972755602ffb7636d03f90e
Neri Sosa <nerisosadev@gmail.com>
2025-12-31 14:57:59 -0600

    feat(order-service): migrate order service to use gRPC microservice transport

--- order-service/src/main.ts
+++ order-service/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ORDERS_PACKAGE_NAME } from './types/proto/orders';
+import { join } from 'path';
+import { Logger } from '@nestjs/common/services/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      package: ORDERS_PACKAGE_NAME,
+      protoPath: [join(__dirname, '../proto/orders.proto')],
+      url: 'localhost:50052',
+    },
+  });
+
+  await app.listen();
+
+  Logger.log('Order microservice is listening...');
 }
 bootstrap();
